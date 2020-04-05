8a7463ecf20b87db58812869bf220b5d5c9340b9
HristakaZ <51200615+HristakaZ@users.noreply.github.com>
2020-04-05 03:14:04 +0300

    Added User Controller
    Added User Controller. Added Register. Added Login (unfinished).

--- Sports News Website/Controllers/UserController.cs
+++ Sports News Website/Controllers/UserController.cs
@@ -7,17 +7,49 @@ using System.Web.Mvc;
 
 namespace Sports_News_Website.Controllers
 {
-    public class UserController : BaseController<Users>
+    public class UsersController : BaseController<Users>
     {
         [HttpGet]
         public ActionResult Register()
         {
             return base.Create();
         }
+
         [HttpPost]
         public ActionResult Register(Users user)
         {
             return base.Create(user);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            SportsNewsDBContext dbContext = new SportsNewsDBContext();
+            List<Users> users = new List<Users>();
+            int id = user.ID;
+            if (users.Exists(x => x.Username == user.Username && x.Password == user.Password))
+            {
+                return RedirectToAction(nameof(Read));
+            }
+            else if (!users.Exists(x => x.Username == user.Username && x.Password == user.Password))
+            {
+                return HttpNotFound();
+            }
+            user = dbContext.Users.Find(id);
+            var session = System.Web.HttpContext.Current.Session;
+            session.Add("userid", user);
+            var sessionUserID = (int)session["userid"];
+            /*if (sessionUserID)
+            {
+                tui za admina (napravi go v atribut)
+            }*/
+            return View(user);
         }
     }
 }
