56c7218c6ccf2ce06d3935146c5158bbebebe4fb
HristakaZ <51200615+HristakaZ@users.noreply.github.com>
2020-04-08 02:48:41 +0300

    Created a Logout
    Created a Logout by using the Session.Remove(adding the session name here); method. Also added a Logout button.

---
+++ Sports News Website/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using Sports_News_Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sports_News_Website.Controllers
+{
+    public class UsersController : BaseController<Users>
+    {
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return base.Create();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Users user)
+        {
+            return base.Create(user);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            SportsNewsDBContext dbContext = new SportsNewsDBContext();
+            if (dbContext.Users.ToList().Exists(x => x.Username == user.Username && x.Password == user.Password) &&
+                ModelState.IsValid)
+            {
+                System.Web.HttpContext.Current.Session["LoginUser"] = user;
+                Session["UserID"] = user.ID;
+                Session["Username"] = user.Username;
+                Session["UserAuthorization"] = user.IsAdmin;
+                return RedirectToAction(nameof(Read));
+            }
+            else if (!dbContext.Users.ToList().Exists(x => x.Username == user.Username && x.Password == user.Password))
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("LoginUser");
+            Session.Remove("UserID");
+            Session.Remove("Username");
+            Session.Remove("UserAuthorization");
+            return RedirectToAction(nameof(Read));
+        }
+    }
+}
