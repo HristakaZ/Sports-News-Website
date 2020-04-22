2f08f93e759ea7ded4492de6c7314e0426197605
HristakaZ <51200615+HristakaZ@users.noreply.github.com>
2020-04-23 02:36:41 +0300

    Attempt at making an image upload.
    TO DO : have to see how to upload the image in a folder located on the local server.

--- Sports News Website/Controllers/NewsController.cs
+++ Sports News Website/Controllers/NewsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,7 @@ using DataAccess.Repositories;
 using DataStructure;
 using Sports_News_Website.CustomAttributes;
 using Sports_News_Website.Services;
+using static System.Net.WebRequestMethods;
 
 namespace Sports_News_Website.Controllers
 {
@@ -27,6 +29,10 @@ namespace Sports_News_Website.Controllers
         [HttpPost]
         public new ActionResult Create(News news)
         {
+            string fileName = Path.GetFileName(news.Photo); // the photo that is uploaded
+            string targetFolder = System.Web.HttpContext.Current.Server.MapPath("~/Photo"); // the folder where the photo needs to go
+            string targetPath = Path.Combine(targetFolder, fileName); // the path that needs to be saved into the db
+            news.Photo = targetPath;
             return base.Create(news);
         }
         [HttpGet]
